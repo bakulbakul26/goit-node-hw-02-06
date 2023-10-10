f39cab052cc2886e051310e167d79687df6793a4
Piotr Bakuliński <pbakul1807@gmail.com>
2023-10-10 18:15:40 +0200

    sprawdzanie

--- middlewares/validateBodyUpd.js
+++ middlewares/validateBodyUpd.js
@@ -1,13 +1,7 @@
-const schemas = require("../schemas/contacts");
-
 const validateBodyUpd = (req, res, next) => {
     const data = req.body;
     if (data && Object.keys(data).length === 0 && data.constructor === Object) {
         return res.status(400).json({ message: "missing fields" });
-    }
-    const { error } = schemas.addSchema.validate(data);
-    if (error) {
-        return res.status(400).json({ message: "missing required name field" });
     }
     next();
 };
--- models/contacts.js
+++ models/contacts.js
@@ -1,79 +1,28 @@
-// const fs = require('fs/promises')
+const { Schema, model } = require("mongoose");
 
-const fs = require("node:fs/promises");
-const path = require("node:path");
-
-const FILE_PATH = path.join(__dirname, "/contacts.json");
-
-const listContacts = async () => {
-    try {
-        const contacts = JSON.parse(await fs.readFile(FILE_PATH, "utf8"));
-        return contacts;
-    } catch (error) {
-        res.status({ error: error.message });
+const contact = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Set name for contact"],
+        },
+        email: {
+            type: String,
+        },
+        phone: {
+            type: String,
+        },
+        favorite: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        versionKey: "revision",
+        timestamps: true,
     }
-};
+);
 
-const getById = async (contactId) => {
-    try {
-        const contacts = JSON.parse(await fs.readFile(FILE_PATH, "utf8"));
-        const contactById = contacts.filter((item) => item.id === contactId);
-        if (!contactById.length) {
-            next();
-        }
-        return contactById[0];
-    } catch (error) {
-        res.status({ error: e.message });
-    }
-};
+const Contacts = model("contacts", contact);
 
-const removeContact = async (contactId) => {
-    try {
-        const contacts = JSON.parse(await fs.readFile(FILE_PATH, "utf8"));
-        const contactById = contacts.filter((item) => item.id === contactId);
-        if (!contactById.length) {
-            next();
-        }
-        const contactsUpdate = contacts.filter((item) => item.id !== contactId);
-        await fs.writeFile(FILE_PATH, JSON.stringify(contactsUpdate));
-    } catch (error) {
-        res.status({ error: e.message });
-    }
-};
-
-const addContact = async (body) => {
-    try {
-        const contacts = JSON.parse(await fs.readFile(FILE_PATH, "utf8"));
-        contacts.push(body);
-        await fs.writeFile(FILE_PATH, JSON.stringify(contacts));
-        return body;
-    } catch (error) {
-        res.status({ error: e.message });
-    }
-};
-
-const updateContact = async (contactId, body) => {
-    try {
-        const contacts = JSON.parse(await fs.readFile(FILE_PATH, "utf8"));
-        const index = contacts.findIndex((item) => item.id === contactId);
-        if (!index) {
-            return next();
-        }
-        contacts[index] = {
-            id: contactId,
-            ...body,
-        };
-        await fs.writeFile(FILE_PATH, JSON.stringify(contacts));
-        return contacts[index];
-    } catch (error) {
-        res.status({ error: e.message });
-    }
-};
-
-module.exports = {
-    listContacts,
-    getById,
-    removeContact,
-    addContact,
-    updateContact,
-};
+module.exports = { Contacts };
