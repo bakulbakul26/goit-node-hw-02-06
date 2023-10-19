763ca9dccfd67b0dc2fb037cf29aab7130c3b950
Piotr Bakuliński <pbakul1807@gmail.com>
2023-10-19 21:55:36 +0200

    sprawdzanie1

--- middlewares/users/validateBodyEmail.js
+++ middlewares/users/validateBodyEmail.js
@@ -1,12 +1,15 @@
 const schemas = require("../../schemas/userVerifyEmail");
 
 const validateBodyEmail = (req, res, next) => {
-    const data = req.body;
-    const { error } = schemas.verifyEmailSchema.validate(data);
-    if (error) {
-        return res.status(400).json({ message: "Error validation Email" });
+    try {
+        const data = req.body;
+        const { error } = schemas.verifyEmailSchema.validate(data);
+        if (error) {
+            return res.status(400).json({ message: "Error validation Email" });
+        }
+    } catch (error) {
+        next();
     }
-    next();
 };
 
 module.exports = validateBodyEmail;
